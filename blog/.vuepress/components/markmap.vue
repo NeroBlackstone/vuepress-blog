202ae5f1ddb4dbe77290a30689aeee09a5f5f12d
Nero Blackstone <905374776@qq.com>
2020-06-08 00:03:33 +0800

    blog update

---
+++ blog/.vuepress/components/markmap.vue
@@ -0,0 +1,25 @@
+<template>
+    <svg id="markmap" style="width: 800px; height: 800px"></svg>
+</template>
+
+<script>
+    import {transform} from 'markmap-lib/dist/transform.common'
+    import {markmap} from 'markmap-lib/dist/view.common'
+
+    export default {
+        name: "markmap",
+        data:function () {
+            return{
+
+            }
+        },
+        mounted() {
+            const data = transform('./note.md');
+            markmap('#markmap', data);
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
